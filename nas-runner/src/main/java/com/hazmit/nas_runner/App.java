5409229a6a500f20e533f5a23b89c943921a99de
Anthony Kitchin <anthony.kitchin@macys.com>
2016-02-15 08:03:56 -0800

    first pass of guess component name

--- nas-runner/src/main/java/com/hazmit/nas_runner/App.java
+++ nas-runner/src/main/java/com/hazmit/nas_runner/App.java
@@ -11,6 +11,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.WordUtils;
 
@@ -37,16 +39,42 @@ public class App {
 		if ("VIDEO_TS".equalsIgnoreCase(file.getName())) {
 			return guessName(file.getParentFile());
 		} else {
-			String name=file.getName().toLowerCase().replaceAll("\\.dvdmedia", "").replaceAll("[^A-Za-z0-9]", " ").trim();
+			String name = file.getName().toLowerCase()
+					.replaceAll("\\.dvdmedia", "")
+					.replaceAll("[^A-Za-z0-9]", " ").trim();
 			name = name.replaceAll("dis[ck]\\s?\\d+$", "");
 			name = name.replaceAll("[s]\\d+\\s?[d]\\d+$", "");
 			name = name.replaceAll("[d]\\d+$", "");
 			return WordUtils.capitalizeFully(name);
 		}
 	}
-	
+
 	public static String guessComponentName(File file) {
-		return file.getAbsolutePath();
+		Pattern p = Pattern.compile("[0-9]+$");
+		if ("VIDEO_TS".equalsIgnoreCase(file.getName())) {
+			return guessComponentName(file.getParentFile());
+		} else {
+			String name = file.getName().toLowerCase()
+					.replaceAll("\\.dvdmedia", "")
+					.replaceAll("[^A-Za-z0-9]", " ").trim();
+			if (name.matches(".+dis[ck]\\s?\\d+$")) {
+				Matcher m = p.matcher(name);
+				if (m.find()) {
+					return "d" + m.group();
+				}
+			}
+			if (name.matches(".+[s]\\d+\\s?[d]\\d+$")) {
+				return name.substring(name.indexOf("s", name.length() - 6),
+						name.length()).replace(" ", "");
+			}
+			if (name.matches(".+[d]\\d+$")) {
+				Matcher m = p.matcher(name);
+				if (m.find()) {
+					return "d" + m.group();
+				}
+			}
+			return "-";
+		}
 	}
 
 	public static List<File> findDVDS(File file) {
@@ -67,12 +95,9 @@ public class App {
 
 		try {
 			System.out.println("Welcome to the NAS Runner");
-			// Sun's ProcessBuilder and Process example
+
 			ProcessBuilder pb = new ProcessBuilder(args[0], "--version");
 			Map<String, String> env = pb.environment();
-			// env.put("VAR1", "myValue");
-			// env.remove("OTHERVAR");
-			// env.put("VAR2", env.get("VAR1") + "suffix");
 			pb.directory(new File(args[1]));
 			Process p = pb.start();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -83,7 +108,6 @@ public class App {
 			}
 
 			Map<String, ShowOrMovie> data = new HashMap<String, ShowOrMovie>();
-
 			List<File> files = findDVDS(new File(args[1]));
 			for (File file : files) {
 				String name = guessName(file);
@@ -93,12 +117,14 @@ public class App {
 				}
 				data.get(name).getComponents().put(componentName, file);
 			}
+			
 			Iterator<Entry<String, ShowOrMovie>> it = data.entrySet()
 					.iterator();
 			while (it.hasNext()) {
 				Entry<String, ShowOrMovie> e = it.next();
 				System.out.println(e.getValue().getName());
-				Iterator<String> it2 = e.getValue().getComponents().keySet().iterator();
+				Iterator<String> it2 = e.getValue().getComponents().keySet()
+						.iterator();
 				while (it2.hasNext()) {
 					System.out.println("\t" + it2.next());
 				}
