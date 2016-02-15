26f2d46e66da989f6a5c155c0ca1e55edbfa4634
Anthony Kitchin <anthony.kitchin@macys.com>
2016-02-14 21:59:44 -0800

    enough for today, detects dvd, guesses names, and executes handbrakecli

---
+++ nas-runner/src/main/java/com/hazmit/nas_runner/ShowOrMovie.java
@@ -0,0 +1,31 @@
+package com.hazmit.nas_runner;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShowOrMovie {
+	public ShowOrMovie(String name) {
+		super();
+		this.name = name;
+	}
+
+	private String name;
+	private Map<String, File> components = new HashMap<String, File>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<String, File> getComponents() {
+		return components;
+	}
+
+	public void setComponents(Map<String, File> components) {
+		this.components = components;
+	}
+}
